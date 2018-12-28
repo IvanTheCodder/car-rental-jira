347b330a4c4f21b15b38e8b605df7030226e58b3
Eugen Stepanenko <yevhen.stepanenko@nure.ua>
2018-12-28 15:01:58 +0200

    'CR-6'

--- src/main/java/ua/nure/stepanenko/SummaryTask4/web/Controller.java
+++ src/main/java/ua/nure/stepanenko/SummaryTask4/web/Controller.java
@@ -28,7 +28,7 @@ public class Controller extends HttpServlet {
         LOG.trace("doGet started");
         process(req, resp);
     }
-//commit1
+//commit12
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
